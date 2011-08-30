b435359e09d23a3ea8c75ead44fa56b9db647ae2
Tom MacWright <tom@macwright.org>
2011-08-30 10:44:19 -0400

    New homepage! Works better!

--- deadsea.js
+++ deadsea.js
@@ -1,41 +1,35 @@
 (function(context) {
     var deadsea = {};
 
-    // From Modest Maps, except with `mode`, because we want to catch
-    // events before any frameworks have the chance.
-    function addEvent(obj, type, fn, mode) {
-        if (obj.attachEvent) {
-            obj['e'+type+fn] = fn;
-            obj[type+fn] = function(){ obj['e'+type+fn](window.event); };
-            obj.attachEvent('on'+type, obj[type+fn]);
-        } else {
-            obj.addEventListener(type, fn, mode);
-            if (type == 'mousewheel') {
-                obj.addEventListener('DOMMouseScroll', fn, mode);
-            }
-        }
-    };
-
-    // From Modest Maps, except doesn't kill the event for the browser -
-    // only stops propagation.
-    function cancelEvent(e) {
-        e.cancelBubble = true;
-        if (e.stopPropagation) { e.stopPropagation(); }
-        return false;
-    };
-
     deadsea.blockScrollInto = function(el) {
+        // This is not compatible with IE.
+        if (!window.addEventListener) return this;
+
+        function isFamily(parent, child) {
+            if (parent === child) return true;
+             var node = child.parentNode;
+             while (node != null) {
+                 if (node === parent) {
+                     return true;
+                 } else {
+                     node = node.parentNode;
+                 }
+             }
+             return false;
+        }
+
         var locked = true;
 
-        addEvent(window, 'mousewheel', function(e) {
-            if (e.target !== el) {
+        window.addEventListener('mousewheel', function(e) {
+            var x = isFamily(el, e.target);
+            if (!x) {
                 locked = true;
-            } else if (e.target === el && locked) {
-                cancelEvent(e);
+            } else if (x && locked) {
+                e.stopPropagation();
             }
         }, true);
 
-        addEvent(el, 'mousemove', function mousemove(e) {
+        el.addEventListener('mousemove', function mousemove(e) {
             locked = false;
         }, false);
 
