5df10895b0892889fba4f575e997ccfb510c073b
Tom MacWright <tom@macwright.org>
2011-09-26 12:21:38 -0400

    README, no more min, FF fixes, package.json, Google Maps example.

---
+++ README.md
@@ -0,0 +1,25 @@
+# deadsea
+
+Deadsea is a minimal library that helps prevent scroll-into fever
+with maps on the internet.
+
+It's abstracted from mapping frameworks; it operates on DOM elements.
+But its tested to work in Modest Maps and Google Maps API v3.
+
+# Compatibility
+
+* Chrome ~16
+* Safari ~5
+* Firefox 3+
+* IE 9+
+
+This code doesn't do anything on IE<9 - it doesn't break the browser,
+but also doesn't fix any problems.
+
+# Authors
+
+* Tom MacWright (tmcw)
+
+# License
+
+* BSD
--- deadsea.js
+++ deadsea.js
@@ -1,33 +1,34 @@
 (function(context) {
     var deadsea = {};
 
+    function isFamily(parent, child) {
+        if (parent === child) return true;
+         var node = child.parentNode;
+         while (node !== null) {
+             if (node === parent) {
+                 return true;
+             } else {
+                 node = node.parentNode;
+             }
+         }
+         return false;
+    }
+
     deadsea.blockScrollInto = function(el) {
+        var locked = true;
         // This is not compatible with IE.
         if (!window.addEventListener) return this;
 
-        function isFamily(parent, child) {
-            if (parent === child) return true;
-             var node = child.parentNode;
-             while (node != null) {
-                 if (node === parent) {
-                     return true;
-                 } else {
-                     node = node.parentNode;
-                 }
-             }
-             return false;
-        }
-
-        var locked = true;
-
-        window.addEventListener('mousewheel', function(e) {
-            var x = isFamily(el, e.target);
-            if (!x) {
+        function handleMouseWheel(e) {
+            if (!isFamily(el, e.target)) {
                 locked = true;
-            } else if (x && locked) {
+            } else if (locked) {
                 e.stopPropagation();
             }
-        }, true);
+        }
+
+        window.addEventListener('mousewheel', handleMouseWheel, true);
+        window.addEventListener('DOMMouseScroll', handleMouseWheel, true);
 
         el.addEventListener('mousemove', function mousemove(e) {
             locked = false;
